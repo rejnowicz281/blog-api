6cf6ad55e3e979dc696e67f48da6e2fb59652b21
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-07 19:56:50 +0200

    Make token expire in 6h instead of 1h

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ const asyncHandler = require("../asyncHandler");
 exports.login = asyncHandler(async (req, res, next) => {
     if (req.body.password === process.env.CMS_PASSWORD) {
         const token = jwt.sign({ password: req.body.password }, process.env.JWT_SECRET, {
-            expiresIn: "1h",
+            expiresIn: "6h",
         });
 
         logger(token);
